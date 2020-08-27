05abe6ee93ad2527d9baeeeb228524ad3bf0f138
DenzelMerrell <denzelmerrell@gmail.com>
2020-08-27 11:56:11 -0400

    Need to make caurosel responsive

---
+++ portfolio.js
@@ -0,0 +1,29 @@
+var distance = $('nav').offset().top,
+    $window = $(window);
+
+// console.log("width: " + $window.width())
+
+// $window.scroll(function () {
+//     if ($window.width() >= parseInt("786px")) {
+//         if ($window.scrollTop() === distance) {
+//             // When the navbar reaches the top of the page.
+//             $('.div_navigation').css("height", "50px");
+//             // $('nav ul').css("margin-top", "5px");
+//             $('nav ul li a').css("font-size", "16px");
+//         }
+//         else {
+//             $('nav').css("height", "30px");
+//             $('nav ul').css("margin-top", "0px");
+//             $('nav ul li a').css("font-size", "12px");
+//         }
+//     }
+// });
+
+
+
+
+
+
+
+
+
