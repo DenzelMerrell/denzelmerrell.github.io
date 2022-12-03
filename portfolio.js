02b97d8f8c6362604f91a8a90673546e2fc9748a
DenzelMerrell <denzelmerrell@gmail.com>
2022-12-03 13:00:41 -0500

    updates

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,20 @@
-let view_all = document.getElementsByClassName('view-all')[0];
+let toggler = document.getElementsByClassName("toggler")[0];
 
-console.log(view_all);
+let div_navigation = document.getElementsByClassName("div_navigation")[0];
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 813)
+        div_navigation.style.zIndex = 0;
+    else
+        div_navigation.style.zIndex = 20;
+});
+
+
+toggler.addEventListener("click", () => {
+    let div_navigation = document.getElementsByClassName("div_navigation")[0];
+    if (!toggler.checked)
+        div_navigation.style.zIndex = 0;
+    else
+        div_navigation.style.zIndex = 20;
+})
+
